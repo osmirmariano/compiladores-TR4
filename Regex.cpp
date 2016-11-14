86b3f03659da279ffd8cbcbf326f39cd7e7f4c4e
osmirmariano <osmirmariano@uft.edu.br>
2016-11-14 19:49:23 -0200

    Inciando a implementação do trabalho 4 de Compiladores

---
+++ Regex.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+class Regex{
+	public:
+		Regex();
+		~Regex();
+		
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Regex"
+
+using namespace std;
+
+int int main(int argc, char const *argv[]){
+	/* code */
+	return 0;
+}
