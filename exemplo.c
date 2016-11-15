61bbd5b73e90085b8896330642f9b9e71f21102a
osmirmariano <osmirmariano@uft.edu.br>
2016-11-15 00:31:44 -0200

    Adiciondo a biblioteca Regex

---
+++ exemplo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "regex.h"
+
+int main()
+{
+	regex_t reg;
+	char expressao[20],palavra[10];
+
+	strcpy(expressao,"^ *([0-9]*[0-9].[0-9]*[0-9])$");
+	strcpy(palavra,"a1234.1234");
+
+	if(regcomp(&reg,expressao,REG_NOSUB)==0) {
+                if(regexec(&reg,palavra,(size_t)0,0,0)==0) {
+                         printf("Palavra reconhecida!\n");
+                }
+                else {
+                         printf("Palavra não reconhecida!\n");
+                }
+	}
+
+	//getchar();
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include "regex.h"
 #include "Regex"
 
 using namespace std;
 
-int int main(int argc, char const *argv[]){
-	/* code */
-	return 0;
-}
+int main(int argc, char const *argv[]){
+    int op;
+
+    
+    do{
+        cout << "---------------------------------------------------" << endl;
+        cout << "\t MENU DE OPÇÃO" << endl;
+        cout << "---------------------------------------------------" << endl;
+        cout << " 1 -- INSERIR EXPRESSÃO REGULAR" << endl;
+        cout << " 2 -- INSERIR TOKENS " << endl;
+        cout << " 3 -- ENTRADA CÓDIGO FONTE" << endl;
+        cout << " 0 -- SAIR" << endl;
+        cout << "---------------------------------------------------" << endl;
+        cout << " OPÇÃO: ";
+        cin >> op;
+        cout << "---------------------------------------------------" << endl;
+
+        switch(op){
+            case 1:
+                cout << "---------------------------------------------------" << endl;
+                cout << "\t INSERÇÃO DA EXPRESSÃO REGULAR" << endl;
+                cout << "---------------------------------------------------" << endl;
+                cout << " EXPRESSÃO: ";
+                break;
+
+            case 2:
+                cout << "---------------------------------------------------" << endl;
+                cout << "\t INSERÇÃO DE TOKENS" << endl;
+                cout << "---------------------------------------------------" << endl;
+                break;
+
+            case 3: 
+                cout << "---------------------------------------------------" << endl;
+                cout << "\t ENTRADA CÓDIGO FONTE" << endl;
+                cout << "---------------------------------------------------" << endl;
+                break;
+                
+            case 0:
+                cout << "\tAPLICAÇÃO ENCERRADA COM SUCESSO!" << endl << endl;
+                break;
+
+            default:
+                cout << "\tOPÇÃO INVÁLIDA, POR FAVOR ESCOLHA UMA VÁLIDA" << endl;
+        }
+        cout << endl << "  PARA VOLTAR AO MENU PRESSIONE ENTER" << endl;
+        cin.ignore().get();//Comando para Pausa no terminal
+        cout << "\e[H\e[2J";//Comando para limpar a tela
+    }while(op != 0);
+};
